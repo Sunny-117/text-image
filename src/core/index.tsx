22fd69a6c6a7a55982fd6c91604e0bcd71b62d2e
Sunny-117 <zhiqiangfu6@gmail.com>
2025-05-31 20:14:05 +0800

    feat: expose createCodeFormatterPlugin

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable react-refresh/only-export-components */
 import { CodeFormatter } from "../core/codeFormatter";
-import { CodeFormatterPlugin } from "../core/plugin";
+import { CodeFormatterPlugin, createCodeFormatterPlugin } from "../core/plugin";
 import { useStreamingSimulator } from "./streamingSimulator";
 import type { StreamingSimulatorConfig } from "./types";
 
-export { CodeFormatter, CodeFormatterPlugin };
+export {
+  CodeFormatter,
+  CodeFormatterPlugin,
+  useStreamingSimulator,
+  createCodeFormatterPlugin,
+};
 export type { StreamingSimulatorConfig };
-
-// eslint-disable-next-line react-refresh/only-export-components
-export { useStreamingSimulator };
