ffe0f062138cc271a56da1de7158af4dd965e862
Sunny-117 <zhiqiangfu6@gmail.com>
2025-05-31 20:09:39 +0800

    feat: expose useStreamingSimulator

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,6 +1,10 @@
 import { CodeFormatter } from "../core/codeFormatter";
 import { CodeFormatterPlugin } from "../core/plugin";
+import { useStreamingSimulator } from "./streamingSimulator";
 import type { StreamingSimulatorConfig } from "./types";
 
 export { CodeFormatter, CodeFormatterPlugin };
 export type { StreamingSimulatorConfig };
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { useStreamingSimulator };
