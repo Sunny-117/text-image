5aacf3548303bac49b4cb927f43c55030ace1920
Sunny-117 <zhiqiangfu6@gmail.com>
2025-05-31 16:59:37 +0800

    feat: sdk vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/core/index.tsx',
+      name: 'code-detector-sdk',
+      fileName: (format) => `code-detector-sdk.${format}.js` // 打包后的文件名
+    },
+    sourcemap: true,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    }
+  }
 })
