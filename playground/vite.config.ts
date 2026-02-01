8bc4ca77fb674be111b7abf431a00f32f2159bee
Sunny-117 <zhiqiangfu6@gmail.com>
2026-02-01 22:06:18 +0800

    chore: deploy

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/text-image',
-  build: {
-    outDir: '../githubPages'
-  }
+  base: '/text-image/',
 })
